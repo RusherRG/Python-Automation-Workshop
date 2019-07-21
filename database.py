123db838f7feb66e841a33c4eea5e10225c2b4bd
RusherRG <rushang101@gmail.com>
2019-07-21 18:14:43 +0530

    RESET

--- database.py
+++ database.py
@@ -19,11 +19,9 @@ def get_unpaid_participants():
     all_participants = list(get_all_participants())
     paid_count = 0
     unpaid = []
-    now = datetime.datetime.now()
     for participant in all_participants:
         if participant['Payment'] == False:
-            if (now - participant['time_filled']).seconds >= 12*60*60:
-                unpaid.append(participant)
+            unpaid.append(participant)
         else:
             paid_count += 1
     return unpaid, paid_count
