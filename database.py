0d8df4a844be9b99c1e1cff147f36c2da6cffc1a
RusherRG <rushang101@gmail.com>
2019-07-21 17:39:14 +0530

    Update datetime

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import os
+import datetime
 mongodb_url = str(os.environ["CODECELL_CHATBOT_MONGODB"])
 
 
@@ -18,9 +19,11 @@ def get_unpaid_participants():
     all_participants = list(get_all_participants())
     paid_count = 0
     unpaid = []
+    now = datetime.datetime.now()
     for participant in all_participants:
         if participant['Payment'] == False:
-            unpaid.append(participant)
+            if (now - participant['time_filled']).seconds >= 12*60*60:
+                unpaid.append(participant)
         else:
             paid_count += 1
     return unpaid, paid_count
